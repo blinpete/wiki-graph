cea38cf6788aecb268d20ac1c9450c9c0ca9fd9e
Peter <pete.blinov@yandex.ru>
2022-06-16 23:51:09 +0300

    update WikiSearch compo

--- src/lib/WikiSearch.svelte
+++ src/lib/WikiSearch.svelte
@@ -1,25 +1,55 @@
 <script lang="ts">
+  // https://www.wikipedia.org
+
   let query = "";
 
-  const suggestions = [];
+  let suggestions = [];
 
-  import { apiClient } from "./apiRequestsClient";
-  import { apiClient as apiClientCustom } from "./apiRequestsCustom";
+  import { apiClient } from "./apiClient";
+  import Suggestions from "./Suggestions.svelte";
 
   async function onKeyup(e: KeyboardEvent) {
     if (e.key !== "Enter") return;
 
     console.log("keyup:", e);
 
-    apiClient.suggest(query);
-    apiClientCustom.suggest(query);
+    // suggestions = await apiClient.suggest(query);
+    suggestions = await apiClient.suggestCustom(query);
+
+    // await apiClient.page(query);
   }
 </script>
 
-<h1>query: {query}</h1>
-<input
-  type="text"
-  name="Wiki Search input"
-  bind:value={query}
-  on:keyup={onKeyup}
-/>
+<div class="input-box">
+  <input
+    type="text"
+    name="Wiki Search input"
+    placeholder="start wiki search..."
+    bind:value={query}
+    on:keyup={onKeyup}
+  />
+  <Suggestions {suggestions} />
+</div>
+
+<style>
+  .input-box {
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+  }
+
+  input {
+    font-size: 1.4rem;
+    color: #444;
+
+    outline: none;
+
+    padding: 4px 10px 6px;
+
+    border: 2px #ccc solid;
+    border-radius: 2px;
+  }
+  input:focus {
+    border-color: #aaa;
+  }
+</style>

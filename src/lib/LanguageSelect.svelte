0e37279b016836e5dfe346d8e9c1f0f53b53e8b1
blinpete <pete.blinov@yandex.ru>
2022-08-28 12:50:00 +0300

    feat: LanguageSelect styling
    keeping it accessible via keyboard

--- src/lib/LanguageSelect.svelte
+++ src/lib/LanguageSelect.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { apiClient } from "./apiClient";
-import IconArrow from "./IconArrow.svelte";
   import { appState, qs } from "./state";
-
+  import IconArrow from "./IconArrow.svelte";
+  
+  
   let selectedLang
   let languages
 
@@ -39,30 +40,92 @@ import IconArrow from "./IconArrow.svelte";
 </script>
 
 <div class="select-wrapper">
-  <label for="searchLanguage">{selectedLang}</label>
-  <select id="searchLanguage" name="language" class="select-lang" bind:value="{selectedLang}">
+  
+
+  <select id="searchLanguage" name="language" class="select--real" bind:value="{selectedLang}">
     {#if languages}
       {#each languages as lang}
         <option value={lang.code}>{lang['*']}</option>
       {/each}
     {/if}
   </select>
+
+  <div class='select--fake'>
+    <label for="searchLanguage">{selectedLang}</label>
+    <IconArrow />
+  </div>
 </div>
 
 
-<style>
+<style lang="postcss">
   .select-wrapper {
     position: absolute;
     right: 1em;
-    top: 20%;
+
+    top: 50%;
+    transform: translateY(-52%);
+
+    font-size: 0.85rem;
+    color: #54595d;
+
+
+    /* width: 3.3em; */
+    /* border: 1px solid hsl(0,0%,75%); */
+
+    &:hover {
+      /* from wikipedia.org */
+      background-color: #f8f9fa;
+    }
   }
 
-  .select-lang {
+  .select--real {
     position: absolute;
+    top: 0;
     right: 0;
-    
+    z-index: 1;
 
-    width: 4em;
+    width: 100%;
+
+    color: inherit;
+
+    appearance: none;
+    opacity: 0;
+    cursor: pointer;
   }
 
+  .select--fake,
+  .select--real {
+    padding: 0.2em 0.3em;
+    
+  }
+
+  .select--fake {
+    width: 100%;
+    border: 1px solid transparent;
+
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & > :global(.icon) {
+      height: 0.7em;
+      width: 0.7em;
+
+      position: relative;
+
+      display: inline-block;
+      margin-left: 3px;
+    }
+
+    & label {
+      text-transform: uppercase;
+    }
+
+  }
+
+  .select--real:focus ~ .select--fake {
+    border: 1px solid blue;
+    /* border-color: blue; */
+  }
 </style>

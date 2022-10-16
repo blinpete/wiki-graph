7c93e531858f7ea0ca16565b0bc3307433950a8d
Peter <pete.blinov@yandex.ru>
2022-10-16 12:58:24 +0300

    chore: lint

--- src/core-anvaka-vs/createRenderer.js
+++ src/core-anvaka-vs/createRenderer.js
@@ -1,45 +1,48 @@
 /**
  * The core of the rendering process
  */
-import createPanZoom from 'panzoom';
-import createTextMeasure from './measureText';
-import createAggregateLayout from './aggregateLayout';
-import bus from './bus';
-import createLinkAnimator from './renderer/linkAnimator';
-import buildLinkIndex from './buildLinkIndex';
+import createPanZoom from "panzoom";
+import createTextMeasure from "./measureText";
+import createAggregateLayout from "./aggregateLayout";
+import bus from "./bus";
+import createLinkAnimator from "./renderer/linkAnimator";
+import buildLinkIndex from "./buildLinkIndex";
 
-import svg from 'simplesvg';
+import svg from "simplesvg";
 
 /**
  * Creates a new renderer. The rendering is done with SVG.
  */
 export default function createRenderer(progress) {
-  const scene = document.querySelector('#scene');
-  const nodeContainer = scene.querySelector('#nodes');
-  const edgeContainer = scene.querySelector('#edges');
+  const scene = document.querySelector("#scene");
+  const nodeContainer = scene.querySelector("#nodes");
+  const edgeContainer = scene.querySelector("#edges");
   const hideTooltipArgs = { isVisible: false };
-  const svgEl = document.querySelector('svg');
+  const svgEl = document.querySelector("svg");
   const pt = svgEl.createSVGPoint();
   const panzoom = createPanZoom(scene);
-  const defaultRectangle = { left: -500, right: 500, top: -500, bottom: 500 }
+  const defaultRectangle = { left: -500, right: 500, top: -500, bottom: 500 };
   panzoom.showRectangle(defaultRectangle);
 
   // maps node id to node ui
   let nodes = new Map();
 
   let linkIndex;
-  let layout, graph, currentLayoutFrame = 0, linkAnimator;
+  let layout,
+    graph,
+    currentLayoutFrame = 0,
+    linkAnimator;
   let textMeasure = createTextMeasure(scene);
-  bus.on('graph-ready', onGraphReady);
+  bus.on("graph-ready", onGraphReady);
 
   return {
     render,
-    dispose
-  }
+    dispose,
+  };
 
   function dispose() {
     clearLastScene();
-    bus.off('graph-ready', onGraphReady);
+    bus.off("graph-ready", onGraphReady);
   }
 
   function onMouseMove(e) {
@@ -54,7 +57,8 @@ export default function createRenderer(progress) {
   function getNearestLink(x, y) {
     if (!linkIndex) return;
 
-    pt.x = x; pt.y = y;
+    pt.x = x;
+    pt.y = y;
     let svgP = pt.matrixTransform(scene.getScreenCTM().inverse());
     let link = linkIndex.findNearestLink(svgP.x, svgP.y, 30);
     if (link) return link.id;
@@ -79,32 +83,32 @@ export default function createRenderer(progress) {
 
   function showTooltip(minLink, clientX, clientY) {
     const { fromId, toId } = minLink.link;
-    bus.fire('show-tooltip', {
+    bus.fire("show-tooltip", {
       isVisible: true,
       from: fromId,
       to: toId,
       x: clientX,
-      y: clientY
+      y: clientY,
     });
 
     removeHighlight();
 
-    nodes.get(fromId).classList.add('hovered');
-    nodes.get(toId).classList.add('hovered');
-    minLink.ui.classList.add('hovered');
+    nodes.get(fromId).classList.add("hovered");
+    nodes.get(toId).classList.add("hovered");
+    minLink.ui.classList.add("hovered");
   }
 
   function hideTooltip() {
-    bus.fire('show-tooltip', hideTooltipArgs);
+    bus.fire("show-tooltip", hideTooltipArgs);
     removeHighlight();
   }
 
   function removeHighlight() {
-    scene.querySelectorAll('.hovered').forEach(removeHoverClass);
+    scene.querySelectorAll(".hovered").forEach(removeHoverClass);
   }
 
   function removeHoverClass(el) {
-    el.classList.remove('hovered');
+    el.classList.remove("hovered");
   }
 
   function render(newGraph) {
@@ -113,15 +117,15 @@ export default function createRenderer(progress) {
 
     layout = createAggregateLayout(graph, progress);
 
-    layout.on('ready', drawLinks);
+    layout.on("ready", drawLinks);
 
     nodes = new Map();
 
     graph.forEachNode(addNode);
-    graph.on('changed', onGraphStructureChanged);
+    graph.on("changed", onGraphStructureChanged);
 
     cancelAnimationFrame(currentLayoutFrame);
-    currentLayoutFrame = requestAnimationFrame(frame)
+    currentLayoutFrame = requestAnimationFrame(frame);
   }
 
   function onGraphReady(readyGraph) {
@@ -133,17 +137,17 @@ export default function createRenderer(progress) {
 
   function frame() {
     if (layout.step()) {
-      currentLayoutFrame = requestAnimationFrame(frame)
+      currentLayoutFrame = requestAnimationFrame(frame);
     }
     updatePositions();
   }
 
   function onGraphStructureChanged(changes) {
-    changes.forEach(change => {
-      if (change.changeType === 'add' && change.node) {
+    changes.forEach((change) => {
+      if (change.changeType === "add" && change.node) {
         addNode(change.node);
       }
-    })
+    });
   }
 
   function drawLinks() {
@@ -170,10 +174,10 @@ export default function createRenderer(progress) {
     clear(nodeContainer);
     clear(edgeContainer);
 
-    document.removeEventListener('mousemove', onMouseMove);
-    svgEl.removeEventListener('click', onSceneClick);
-    if (layout) layout.off('ready', drawLinks);
-    if (graph) graph.off('changed', onGraphStructureChanged);
+    document.removeEventListener("mousemove", onMouseMove);
+    svgEl.removeEventListener("click", onSceneClick);
+    if (layout) layout.off("ready", drawLinks);
+    if (graph) graph.off("changed", onGraphStructureChanged);
     if (linkAnimator) linkAnimator.dispose();
   }
 
@@ -200,22 +204,22 @@ export default function createRenderer(progress) {
       height: uiAttributes.height,
       rx: uiAttributes.rx,
       ry: uiAttributes.ry,
-      fill: 'white',
-      'stroke-width': uiAttributes.strokeWidth,
-      stroke: '#58585A'
-    }
+      fill: "white",
+      "stroke-width": uiAttributes.strokeWidth,
+      stroke: "#58585A",
+    };
     const textAttributes = {
-      'font-size': uiAttributes.fontSize,
+      "font-size": uiAttributes.fontSize,
       x: uiAttributes.px,
-      y: uiAttributes.py
-    }
+      y: uiAttributes.py,
+    };
 
-    const rect = svg('rect', rectAttributes);
-    const text = svg('text', textAttributes)
+    const rect = svg("rect", rectAttributes);
+    const text = svg("text", textAttributes);
     text.text(node.id);
 
-    const ui = svg('g', {
-      transform: `translate(${pos.x}, ${pos.y})`
+    const ui = svg("g", {
+      transform: `translate(${pos.x}, ${pos.y})`,
     });
     ui.appendChild(rect);
     ui.appendChild(text);
@@ -223,12 +227,12 @@ export default function createRenderer(progress) {
     nodeContainer.appendChild(ui);
     nodes.set(node.id, ui);
 
-
-
-
+    // --------------------- listeners ----------------------
+    let moved;
+    let moveListener = (e) => {
     // --------------------- listeners ---------------------- 
     let moved
-    let moveListener = e => {
+
       moved = true
     }
 
@@ -239,6 +243,8 @@ export default function createRenderer(progress) {
       ui.addEventListener('mousemove', moveListener)
     }
     let upListener = e => {
+    };
+    let upListener = (e) => {
       if (moved) {
         // console.log('moved')
       } else {
@@ -246,38 +252,39 @@ export default function createRenderer(progress) {
         // console.log('not moved')
       }
 
+      moved = false;
       moved = false
       ui.removeEventListener('mousemove', moveListener)
     }
+    };
 
     // click
     ui.addEventListener('mousedown', downListener)
     ui.addEventListener('mouseup', upListener)
 
     // right click
-    ui.addEventListener('contextmenu', e => {
+    ui.addEventListener("contextmenu", (e) => {
       // console.log('[RightClick] event:', e)
 
       if (e.button == 2) {
-        e.preventDefault()
-        bus.fire('node-click-right', { node })
+        e.preventDefault();
+        bus.fire("node-click-right", { node });
       }
     });
 
-
     // enter, leave
-    ui.addEventListener('mouseenter', e => {
+    ui.addEventListener("mouseenter", (e) => {
       // cancel on drag
-      if (moved) return
+      if (moved) return;
 
-      onEnterNode(e, node)
-    })
-    ui.addEventListener('mouseleave', e => onLeaveNode(e, node))
+      onEnterNode(e, node);
+    });
+    ui.addEventListener("mouseleave", (e) => onLeaveNode(e, node));
   }
 
   function onNodeClick(e, node) {
-    // console.log("🚀 ~ onNodeClick ~ e, node", e, node)
-    bus.fire('show-details-node', { node })
+    console.log("🚀 ~ onNodeClick ~ e, node", e, node);
+    bus.fire("show-details-node", { node });
   }
 
   function onLeaveNode(e, node) {
@@ -285,40 +292,39 @@ export default function createRenderer(progress) {
     removeHighlight();
 
     // tooltip
-    bus.fire('show-tooltip-node', { node: null })
+    bus.fire("show-tooltip-node", { node: null });
   }
 
   function onEnterNode(e, node) {
     // console.log("🚀 ~ onHoverNode ~ e", e.target)
     // console.log("🚀 ~ onHoverNode ~ node", node)
 
-    const el = e.target
-    addHoveredClass(el)
+    const el = e.target;
+    addHoveredClass(el);
 
     if (node.links?.length) {
-      node.links.forEach(link => {
+      node.links.forEach((link) => {
         // console.log("🚀 ~ onHoverNode ~ link", link)
-        const linkObj = linkAnimator.getLinkInfo(link.id)
-        addHoveredClass(linkObj.ui)
+        const linkObj = linkAnimator.getLinkInfo(link.id);
+        addHoveredClass(linkObj.ui);
 
-        const linkedId = link.fromId !== node.id ? link.fromId : link.toId
-        const linkedNode = nodes.get(linkedId)
-        addHoveredClass(linkedNode)
-      })
+        const linkedId = link.fromId !== node.id ? link.fromId : link.toId;
+        const linkedNode = nodes.get(linkedId);
+        addHoveredClass(linkedNode);
+      });
     }
 
     function addHoveredClass(htmlEl) {
-      htmlEl.classList.add('hovered')
+      htmlEl.classList.add("hovered");
     }
 
     // tooltip
-    bus.fire('show-tooltip-node', {
+    bus.fire("show-tooltip-node", {
       node,
       x: e.clientX,
       y: e.clientY,
-    })
+    });
   }
-
 
   function getNodeUIAttributes(nodeId, dRatio) {
     const fontSize = 45 * dRatio + 14;
@@ -336,14 +342,14 @@ export default function createRenderer(progress) {
       ry: 15 * dRatio + 2,
       px: -width / 2 + size.spaceWidth * 3,
       py: -height / 2 + fontSize * 1.1,
-      strokeWidth: 4 * dRatio + 1
+      strokeWidth: 4 * dRatio + 1,
     };
   }
 
   function updatePositions() {
     nodes.forEach((ui, nodeId) => {
-      let pos = getNodePosition(nodeId)
-      ui.attr('transform', `translate(${pos.x}, ${pos.y})`);
+      let pos = getNodePosition(nodeId);
+      ui.attr("transform", `translate(${pos.x}, ${pos.y})`);
     });
   }
 
